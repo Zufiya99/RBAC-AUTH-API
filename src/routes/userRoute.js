7208b805d232389c2aae0dfca61b4dd4ec914702
Zufiya Idrisi <idrisizufiya@gmail.com>
2025-01-18 16:34:20 +0530

    Completed RBAC API

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,21 +1,32 @@
 import express from "express";
 import verifyToken from "../middlewares/authMiddleware.js";
+import authorizeRoles from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
 // Only admin can access this route
-router.get("/admin", verifyToken, (req, res) => {
+router.get("/admin", verifyToken, authorizeRoles("admin"), (req, res) => {
   res.json({ message: "Welcome admin" });
 });
 
 // Both admin and manager can access this route
-router.get("/manager", verifyToken, (req, res) => {
-  res.json({ message: "Welcome manager" });
-});
+router.get(
+  "/manager",
+  verifyToken,
+  authorizeRoles("admin", "manager"),
+  (req, res) => {
+    res.json({ message: "Welcome manager" });
+  }
+);
 
 // All users can access this route
-router.get("/user", verifyToken, (req, res) => {
-  res.json({ message: "Welcome user" });
-});
+router.get(
+  "/user",
+  verifyToken,
+  authorizeRoles("admin", "manager", "user"),
+  (req, res) => {
+    res.json({ message: "Welcome user" });
+  }
+);
 
 export default router;
